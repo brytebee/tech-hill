5b2738248aa952c73d57f612ce077b7551786383
Bright Atsighi <brytebee@gmail.com>
2026-04-09 18:37:55 +0100

    fix: mount Sonner Toaster in providers to enable UI feedback

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { LoaderProvider } from "@/hooks/use-loader";
 import { ModalProvider } from "@/hooks/use-modal";
+import { Toaster } from "sonner";
 import { GlobalLoader } from "@/components/ui/global-loader";
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -14,6 +15,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
           <ModalProvider>
             {children}
             <GlobalLoader />
+            <Toaster richColors position="top-center" />
           </ModalProvider>
         </LoaderProvider>
       </ThemeProvider>
